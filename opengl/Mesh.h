d68ddf3b31dffc732669a1c876c4942284ab17e2
anchoringpoint <108213095+anchoringpoint@users.noreply.github.com>
2023-08-31 18:02:29 +0800

    refractor

--- opengl/Mesh.h
+++ opengl/Mesh.h
@@ -6,14 +6,18 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "shader_m.hpp"
+#include "shader_m.h"
+
 #include <vector>
 using std::string, std::vector,std::max;
 
 
 struct Vertex {
+    // position
     glm::vec3 Position;
+    // normal
     glm::vec3 Normal;
+    // texCoords
     glm::vec2 TexCoords;
 
 };
@@ -26,92 +30,95 @@ struct Texture {
 
 class Mesh {
 public:
-    //Mesh的数据
+    // mesh Data
     vector<Vertex>       vertices;
     vector<unsigned int> indices;
     vector<Texture>      textures;
     unsigned int VAO;
 
-    //构造函数
+    // constructor
     Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
     {
         this->vertices = vertices;
         this->indices = indices;
         this->textures = textures;
 
-		//设置Mesh的渲染状态
+        // now that we have all the required data, set the vertex buffers and its attribute pointers.
         setupMesh();
     }
 
-    //渲染函数
+    // render the mesh
     void Draw(Shader& shader)
     {
+        // bind appropriate textures
         unsigned int diffuseNr = 1;
         unsigned int specularNr = 1;
         unsigned int normalNr = 1;
         unsigned int heightNr = 1;
         for (unsigned int i = 0; i < textures.size(); i++)
         {
-            glActiveTexture(GL_TEXTURE0 + i); //在绑定前激活对应材质单元
-            //number用于表示对应材质的当前索引数
+            glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+            // retrieve texture number (the N in diffuse_textureN)
             string number;
 
             string name = textures[i].type;
             if (name == "texture_diffuse")
                 number = std::to_string(diffuseNr++);
             else if (name == "texture_specular")
-                number = std::to_string(specularNr++); 
+                number = std::to_string(specularNr++); // transfer unsigned int to string
             else if (name == "texture_normal")
-                number = std::to_string(normalNr++); 
+                number = std::to_string(normalNr++); // transfer unsigned int to string
             else if (name == "texture_height")
-                number = std::to_string(heightNr++); 
+                number = std::to_string(heightNr++); // transfer unsigned int to string
 
-            //设置材质到着色器
-			shader.setInt(("material." + name + "[" + number + "]").c_str(), i);
+            // now set the sampler to the correct texture unit
+            glUniform1i(glGetUniformLocation(shader.ID, ("material."+name + "["+number+"]").c_str()), i);
             shader.setInt(string("material.") + "texture_diffuse_num", diffuseNr);
             shader.setInt(string("material.") + "texture_specular_num", specularNr);
-
-            //绑定材质
+            // and finally bind the texture
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
         }
 
-        //渲染Mesh
+        // draw mesh
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
-		//激活默认材质
+        // always good practice to set everything back to defaults once configured.
         glActiveTexture(GL_TEXTURE0);
     }
 
 private:
-    //渲染所用数据
+    // render data 
     unsigned int VBO, EBO;
 
-	//初始化Mesh的渲染数据
+    // initializes all the buffer objects/arrays
     void setupMesh()
     {
-        //创建buffers/arrays
+        // create buffers/arrays
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
         glGenBuffers(1, &EBO);
 
         glBindVertexArray(VAO);
+        // load data into vertex buffers
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-        //由于vector结构在内存中连续，故可直接使用地址存入
+        // A great thing about structs is that their memory layout is sequential for all its items.
+        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
+        // again translates to 3/2 floats which translates to a byte array.
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-        // 设置顶点位置
+        // set the vertex attribute pointers
+        // vertex Positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        //设置法线
+        // vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        //设置材质坐标
+        // vertex texture coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
     }
